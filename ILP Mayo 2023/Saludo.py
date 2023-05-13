ee1e7454bec14fadecc49bc2d0d8be2e75192c47
sitesystem <l_aloaxa05@hotmail.com>
2023-05-13 09:09:09 -0600

    1ra subida de archivos de Python

---
+++ ILP Mayo 2023/Saludo.py
@@ -0,0 +1,11 @@
+# Ejemplo 1. SALUDO
+# COMENTARIOS: Sirven para detallar o describir (documentar) cada parte del código o programa
+# A los comentarios los omite o no los toma en cuenata Python
+
+# Imprimiendo o mostrando en pantalla los resultados
+print("Hola Mundo") # Saludo
+print('Eduardo'); # Imprimir el Nombre
+print("Suma = ", 9+6) # Suma
+print("Resta = ", 10-5)
+print(2*3);
+print(10/5)
---
+++ ILP Mayo 2023/TiposDeDatos.py
@@ -0,0 +1,8 @@
+# Tipos de Datos: Clasificación de los datos
+
+# Declarar las variables
+nombre = "Eduardo"
+edad = 31
+dirección = "Calle X, Mun. Y, C.P. Z"
+teléfono = "5523235261"
+jjljklfe
